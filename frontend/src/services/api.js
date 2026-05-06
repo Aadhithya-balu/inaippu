ebb6d4573b62d86ec308648cdc45eecac353c12c
Aadhi <aadhithyabalu05@gmail.com>
2026-05-06 21:41:38 +0530

    fix: hardcode render URL directly in api.js and LocationPicker

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
-import { API_URL } from '../config';
 
-const api = axios.create({ baseURL: API_URL });
+const api = axios.create({
+  baseURL: 'https://inaippu-sr3w.onrender.com/api',
+});
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
